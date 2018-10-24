1326f50a7a65e00acc9253a4d5df685de94362aa
Abi Raskin <abiraskin@gmail.com>
2018-10-24 12:03:55 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,6 @@ def speak_to_grandma(phrase)
   if phrase != phrase.upcase()
     return "HUH?! SPEAK UP, SONNY!"
   elsif
-    return NO, NOT SINCE 1938!
+    return "NO, NOT SINCE 1938!"
   end
 end
